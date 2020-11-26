847d2f12b756cdcd6c551e92ffbbb8c1e828b1ae
joelsprangers <68640121+joelsprangers@users.noreply.github.com>
2020-11-26 15:54:13 +0100

    finished exercise

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const ikRockArrowFunctions = () => console.log("Joe, ik rock de arrow functions!");
+const fivePlusSeven = () => 5 + 7;
+
+const myFunction = (a, b) => a + b;
+const addFive = a => a + 5;
+
+const createObject = () => ({greeting: "hoi"})
+
+fivePlusSeven();
+ikRockArrowFunctions()
+
+console.log(fivePlusSeven());
